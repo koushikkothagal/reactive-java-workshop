c3dc7d37242e68fdc2ebbd464fe889846f306c6e
Z007JB2 <sripreethi.puchakayala@target.com>
2023-09-03 12:58:15 +0530

    exercise 4 done - Mono

--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class Exercise4 {
 
@@ -8,11 +9,23 @@ public class Exercise4 {
 
         // Use ReactiveSources.intNumberMono()
 
-        // Print the value from intNumberMono when it emits
+
         // TODO: Write code here
+        // NOTE : non-blocking call
+        ReactiveSources.intNumberMono().subscribe(number -> System.out.println(number));
 
         // Get the value from the Mono into an integer variable
-        // TODO: Write code here
+        // NOTE : blocking call
+        Integer number = ReactiveSources.intNumberMono().block();
+        System.out.println("After blocking: " + number);
+
+        // OR
+
+        Optional<Integer> numberOptional = ReactiveSources.intNumberMono().blockOptional();
+        System.out.println("After blocking optional: " + (numberOptional.isPresent() ? numberOptional.get() : 0));
+
+        // NOTE : Mono can return  a list
+
 
         System.out.println("Press a key to end");
         System.in.read();
